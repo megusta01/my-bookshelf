9dd3f5944da862b5eee4ade5459b1a454a63bef5
CarlHenry670 <capic.2023118tads0035@aluno.ifpi.edu.br>
2024-01-22 19:52:51 -0300

    correção no redirecionamento

--- login/assets/js/index.js
+++ login/assets/js/index.js
@@ -6,7 +6,7 @@ if (localStorage.getItem("token") == null) {
 const userLogado = JSON.parse(localStorage.getItem("userLogado"));
 
 const logado = document.querySelector("#logado");
-logado.innerHTML = `Olá ${userLogado.nome}`;
+logado.innerHTML = `Olá, ${userLogado.nome}`;
 
 function sair() {
   localStorage.removeItem("token");
